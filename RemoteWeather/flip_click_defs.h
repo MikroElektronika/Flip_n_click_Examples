c3a5495590f44baa8af1674e9c4cc337b721ed3c
Milos Vidojevic <mvidojevic82@gmail.com>
2017-09-27 16:32:09 +0200

    Update flip_click_defs.h

--- RemoteWeather/flip_click_defs.h
+++ RemoteWeather/flip_click_defs.h
@@ -1,60 +1,22 @@
-/*******************************************************************************
-* Title                 :   Flip and Click Pin Definitions
-* Filename              :   flip_click_defs.h
-* Author                :   RL
-* Origin Date           :   04/10/2015
-* Notes                 :   Running on Flip and Click Due
-*******************************************************************************/
-/*************** MODULE REVISION LOG ******************************************
-*
-*    Date    Software Version    Initials   Description
-*  04/10/2015    .1               RL        Module Created.
-*
-*******************************************************************************/
 #ifndef FLIP_CLICK_DEFS_H
 #define FLIP_CLICK_DEFS_H
 
 /***************
- ***  LEDS  ***
- **************/
-#define LEDA 38
-#define LEDB 37
-#define LEDC 39
-#define LEDD 40
-
-/***************
- *** Click A ***
+ *** Click D ***
  ***************/
-#define A_AN   54
-#define A_RST  33
-#define A_CS   77
-#define A_SCK  76
-#define A_MISO 74
-#define A_MOSI 75
+#define D_AN   57
+#define D_RST  36
+#define D_CS   78
+#define D_SCK  76
+#define D_MISO 74
+#define D_MOSI 75
 
-#define A_PWM  6
-#define A_INT  26
-#define A_RX   15
-#define A_TX   14
-#define A_I2C0_SCL 71
-#define A_I2C0_SDA 70
-
-/***************
- *** Click B ***
- ***************/
-#define B_AN   55
-#define B_RST  34
-#define B_CS   4
-#define B_SCK  76
-#define B_MISO 74
-#define B_MOSI 75
-
-#define B_PWM  7
-#define B_INT  27
-#define B_RX   15
-#define B_TX   14
-#define B_I2C0_SCL 71
-#define B_I2C0_SDA 70
+#define D_PWM  9
+#define D_INT  29
+#define D_RX   15
+#define D_TX   14
+#define D_I2C1_SCL 21
+#define D_I2C1_SDA 20
 
 /***************
  *** Click C ***
@@ -74,20 +36,37 @@
 #define C_I2C1_SDA 20
 
 /***************
- *** Click D ***
+ *** Click B ***
  ***************/
-#define D_AN   57
-#define D_RST  36
-#define D_CS   78
-#define D_SCK  76
-#define D_MISO 74
-#define D_MOSI 75
+#define B_AN   55
+#define B_RST  34
+#define B_CS   4
+#define B_SCK  76
+#define B_MISO 74
+#define B_MOSI 75
 
-#define D_PWM  9
-#define D_INT  29
-#define D_RX   15
-#define D_TX   14
-#define D_I2C1_SCL 21
-#define D_I2C1_SDA 20
+#define B_PWM  7
+#define B_INT  27
+#define B_RX   15
+#define B_TX   14
+#define B_I2C1_SCL 71
+#define B_I2C1_SDA 70
+
+/***************
+ *** Click A ***
+ ***************/
+#define A_AN   54
+#define A_RST  33
+#define A_CS   77
+#define A_SCK  76
+#define A_MISO 74
+#define A_MOSI 75
+
+#define A_PWM  6
+#define A_INT  26
+#define A_RX   15
+#define A_TX   14
+#define A_I2C1_SCL 71
+#define A_I2C1_SDA 70
 
 #endif
